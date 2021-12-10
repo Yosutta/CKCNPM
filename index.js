819750621804afc8cf454b6c0faeaeacf1992a8a
Yosutta <phuphuongtin@gmail.com>
2021-12-10 22:40:06 +0700

    Added POST route to login form

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require("express")
 const app = express()
 
+const dbUrl = 'mongodb://127.0.0.1:27017/CNPM'
+
+const mongoose = require('mongoose')
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log("CONNECTED!!!")
+    })
+    .catch((err)=>{
+        console.log("CONNECTION FAILED!!!")
+        console.log(err)
+    })
+
+const User = require("./models/user")
+
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 
 app.get("/login",(req,res)=>{
     res.render("auth/login")
 })
-
-app.post("/login",(req,res)=>{
-    console.log(req.body)
-    res.send(req.body)
+ 
+app.post("/login",async(req,res)=>{
+    const foundUser = await User.findOne({"username" : req.body["username"]});
+    let message = "We can not find any profile with that username or password"
+    if(foundUser){
+        message = "Welcome back"
+    }
+    res.send(message)
 })
 
 app.get("*", (req,res)=>{
