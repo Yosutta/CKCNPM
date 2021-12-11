f88d75619da1b7a51492fd17e9b7b23e7c885ae0
Yosutta <phuphuongtin@gmail.com>
2021-12-11 17:01:13 +0700

    Completed basic operation for accountant login and registration

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const app = express()
+const Accountant = require("./models/accountant")
 
 const dbUrl = 'mongodb://127.0.0.1:27017/CNPM'
-
 const mongoose = require('mongoose')
 mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>{
@@ -13,26 +13,48 @@ mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
         console.log(err)
     })
 
-const User = require("./models/user")
-
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 
-app.get("/login",(req,res)=>{
-    res.render("auth/login")
+app.get('/home',(req,res)=>{
+    res.render('auth/home')
+})
+
+app.get("/accountant", (req,res)=>{
+    res.redirect("/accountant/login")
+})
+
+app.get("/accountant/login",(req,res)=>{
+    res.render("auth/accountant/login")
 })
  
-app.post("/login",async(req,res)=>{
-    const foundUser = await User.findOne({"username" : req.body["username"]});
+app.post("/accountant/login",async(req,res)=>{
+    const foundAccountant = await Accountant.findOne({"username" : req.body["username"]});
     let message = "We can not find any profile with that username or password"
-    if(foundUser){
-        message = "Welcome back"
+    console.log(foundAccountant)
+    if(foundAccountant){
+        if(foundAccountant['password'] === req.body['password']){
+            message = "Welcome back"
+        }
+    }
+    else{
+        message = "We can not find any profile with that username or password"
     }
     res.send(message)
 })
 
+app.get("/accountant/register", (req,res)=>{
+    res.render("auth/accountant/register")
+})
+
+app.post("/accountant/register", async (req,res)=>{
+    const newAccountant = await new Accountant(req.body)
+    await newAccountant.save()
+    res.redirect('/accountant/login');
+})
+
 app.get("*", (req,res)=>{
-    res.send("Invalid url")
+    res.redirect('/home')
 })
 
 app.listen("8080", (req,res)=>{
