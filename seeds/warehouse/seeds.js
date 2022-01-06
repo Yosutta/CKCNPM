a6a42dc63d813fd6db497aa972e11438d46b0636
Yosutta <phuphuongtin@gmail.com>
2022-01-06 15:26:24 +0700

    Updated a data seeding file

--- seeds/warehouse/seeds.js
+++ seeds/warehouse/seeds.js
@@ -1,36 +1,42 @@
 const dbUrl = 'mongodb://127.0.0.1:27017/CNPM'
 const mongoose = require('mongoose')
-mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>{
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
         console.log("CONNECTED!!!")
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.log("CONNECTION FAILED!!!")
         console.log(err)
     })
 
 const Warehouse = require('../../models/warehouse')
-const {products} = require('./drugs.js')
+const Import = require('../../models/import')
+const Order = require('../../models/order')
 
-const seeddb = async()=>{
+const { products } = require('./drugs.js')
+
+const seeddb = async () => {
     await Warehouse.deleteMany()
+    await Order.deleteMany()
+    await Import.deleteMany()
+
     let count = 0
-    for(let i=0; i<products.length;i++){
-        const quantityRand = Math.floor(Math.random()*101) *10
-        const priceRand = Math.floor(Math.random()*8) + Math.floor(Math.random()*10)*0.1 + 0.09 + 3
+    for (let i = 0; i < products.length; i++) {
+        const quantityRand = Math.floor(Math.random() * 101) * 10
+        const priceRand = (Math.floor(Math.random() * 8) + Math.floor(Math.random() * 10) * 0.1 + 0.09 + 3).toFixed(2)
         const productManufacturer = products[i]['labeler_name']
         const productName = products[i]['brand_name']
 
         const test = ``
-        const foundProduct = await Warehouse.find({'name': { $regex : new RegExp(productName, "i") }})
-        if(foundProduct.length===0){
+        const foundProduct = await Warehouse.find({ 'name': { $regex: new RegExp(productName, "i") } })
+        if (foundProduct.length === 0) {
             const newProduct = await new Warehouse({
                 name: productName,
                 quantity: quantityRand,
                 price: priceRand,
                 manufacturer: productManufacturer
             })
-            count+=1
+            count += 1
             console.log(count)
             await newProduct.save()
         }
@@ -38,6 +44,6 @@ const seeddb = async()=>{
     }
 }
 
-seeddb().then(()=>{
+seeddb().then(() => {
     mongoose.disconnect()
 })
