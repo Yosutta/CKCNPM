dab8774cb66c55e218ba0c183ef15a45310e0177
Yosutta <phuphuongtin@gmail.com>
2022-01-12 19:04:14 +0700

    Added new frontend and backend for viewing imports and exports history

---
+++ public/scripts/viewrequests/viewexports.js
@@ -0,0 +1,57 @@
+function getData(month) {
+    return $.ajax({
+        method: "GET",
+        url: "/accountant/revenue/exports/get",
+        data: { month: month },
+        success: function (data) {
+            fillData(data['result'])
+        }
+    })
+}
+
+async function fillData(data) {
+    const exports = data
+    console.log(exports)
+    const table = document.querySelector("#list")
+    let totalPrice = 0
+
+    for (var i = table.rows.length - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+
+    for (let i = 0; i < exports.length; i++) {
+        let newRow = table.insertRow(i + 1)
+
+        let exportId = newRow.insertCell(0)
+        let exportRetailerName = newRow.insertCell(1)
+        let exportRetailerPhoneNumber = newRow.insertCell(2)
+        let exportDate = newRow.insertCell(3)
+        let exportPrice = newRow.insertCell(4)
+
+        let price = 0
+        for (let j = 0; j < exports[i].products.length; j++) {
+            price += parseInt(exports[i].products[j].item.price) * exports[i].products[j].orderedQuantity
+        }
+
+        console.log(exports)
+        exportId.innerHTML = exports[i]['_id']
+        exportRetailerName.innerHTML = exports[i]['order_id']['retailer_id']['fullname']
+        exportRetailerPhoneNumber.innerHTML = exports[i]['order_id']['delivery']['phonenumber']
+        let date = new Date(exports[i]['exportDate'])
+        exportDate.innerHTML = date.toDateString()
+        exportPrice.innerHTML = price + '$'
+
+        totalPrice += price
+    }
+    console.log(totalPrice)
+    document.querySelector('#total').innerHTML = 'Total price: ' + totalPrice + '$'
+}
+
+const monthChooser = document.querySelector("#monthSelector")
+monthChooser.addEventListener('change', () => {
+    getData(monthChooser.value)
+})
+
+document.querySelector('#exportSwitch').addEventListener('change', () => {
+    window.location.href = "/accountant/revenue/imports";
+})
---
+++ public/scripts/viewrequests/viewimports.js
@@ -0,0 +1,51 @@
+function getData(month) {
+    return $.ajax({
+        method: "GET",
+        url: "/accountant/revenue/imports/get",
+        data: { month: month },
+        success: function (data) {
+            fillData(data['result'])
+        }
+    })
+}
+
+async function fillData(data) {
+    const imports = data
+    const table = document.querySelector("#list")
+    let totalPrice = 0
+
+    for (var i = table.rows.length - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+
+    for (let i = 0; i < imports.length; i++) {
+        let newRow = table.insertRow(i + 1)
+
+        let importId = newRow.insertCell(0)
+        let importDate = newRow.insertCell(1)
+        let importPrice = newRow.insertCell(2)
+
+        let price = 0
+        for (let j = 0; j < imports[i].products.length; j++) {
+            price += parseInt(imports[i].products[j].item.price) * imports[i].products[j].importQuantity
+        }
+
+        importId.innerHTML = imports[i]['_id']
+        let date = new Date(imports[i]['importDate'])
+        importDate.innerHTML = date.toDateString()
+        importPrice.innerHTML = price + '$'
+
+        totalPrice += price
+    }
+    console.log(totalPrice)
+    document.querySelector('#total').innerHTML = 'Total price: ' + totalPrice + '$'
+}
+
+const monthChooser = document.querySelector("#monthSelector")
+monthChooser.addEventListener('change', () => {
+    getData(monthChooser.value)
+})
+
+document.querySelector('#importSwitch').addEventListener('change', () => {
+    window.location.href = "/accountant/revenue/exports";
+})
